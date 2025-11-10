17a7812cff5b5834a2bc984f23d33d7e95e788eb
Katie Zhu <zhu.k@wustl.edu>
2025-11-09 18:00:22 -0600

    rendering of Square components with handleClick()

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,17 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[0]} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
